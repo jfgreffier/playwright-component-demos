9b366148a55f6f98c72b89249172f72c7a39d211
Jean-François Greffier <jfgreffier@users.noreply.github.com>
2024-04-22 20:20:11 +0200

    refacto: simplify Counter.spec.tsx

--- src/Counter.spec.tsx
+++ src/Counter.spec.tsx
@@ -1,7 +1,7 @@
 import { expect as playwrightExpect, test } from '@playwright/experimental-ct-react';
 import Counter from './Counter';
 
-// very short expect timeout, more confortable if you try TDD
+// very short expect timeout
 const expect = playwrightExpect.configure({ timeout: 1 });
 
 // Web-first assertions
@@ -9,18 +9,4 @@ test('renders', async ({ mount }) => {
   const component = await mount(<Counter />);
 
   // TODO check it is visible
-});
-
-// Locator, auto-waiting
-test('increments', async ({ mount }) => {
-  const component = await mount(<Counter />);
-
-  // TODO
-});
-
-// Testing Library queries, debugging
-test('decrements', async ({ mount }) => {
-  const component = await mount(<Counter />);
-
-  // TODO
 });
